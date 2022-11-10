2d697b8a9600af7c7d2f3b313f1d317cbbe99c8b
jdbrayshaw629 <jdbrayshaw629@gmail.com>
2022-11-10 13:23:50 -0500

    Added a few dependencies for pd.read_html() and started working on importing the data. Added a TODO for a ValueError on read_html(), should just need to format HTML file possibly.

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,4 +1,12 @@
-import pandas
+import pandas as pd
+import bs4
+
+
+def main():
+    converted_data_list = pd.read_html('casts.html')  # TODO: fix ValueError: no tables found
+    print(converted_data_list)
+    return
+
 
 if __name__ == "__main__":
-    print("WRITE ME")
+    main()
